8dfffa47c17d2b702142952881a99a5b74a7402f
TruKoaiu <patryk.siekierski03@gmail.com>
2025-02-04 18:17:01 +0100

    Working on login page
    Quite bad for now, but i'm working on it XD

--- dorm-assist-website/src/components/LoginAndRegister/loginForm.jsx
+++ dorm-assist-website/src/components/LoginAndRegister/loginForm.jsx
@@ -1,55 +1,56 @@
 import { useForm } from "react-hook-form";
 import axios from "axios";
+// import Navbar from "../components/Universal/navbar";
 import { useNavigate } from "react-router-dom";
+import Navbar from "../Universal/navbar";
+// import Navbar from "../Universal/navbar";
 
 export default function LoginForm() {
-  const { register, handleSubmit } = useForm();
-  const navigate = useNavigate();
-  const onSubmit = (data) => {
-    loginAndGetToken(data);
-  };
+  // const { register, handleSubmit } = useForm();
+  // const navigate = useNavigate();
+  // const onSubmit = (data) => {
+  //   loginAndGetToken(data);
+  // };
 
-  async function loginAndGetToken(body) {
-    console.log(body);
+  // async function loginAndGetToken(body) {
+  //   console.log(body);
 
-    try {
-      const tokenData = await axios({
-        method: "post",
-        url: "http://localhost:8080/users/authenticate",
-        data: body,
-      });
+  //   try {
+  //     const tokenData = await axios({
+  //       method: "post",
+  //       url: "http://localhost:8080/users/authenticate",
+  //       data: body,
+  //     });
 
-      // console.log(tokenData.headers.get("Content-Type"));
-      if (!tokenData.headers.get("Content-Type").includes("html")) {
-        const token = tokenData.data;
-        localStorage.setItem("token", token);
-        console.log("Token: " + token);
-      }
+  //     if (!tokenData.headers.get("Content-Type").includes("html")) {
+  //       const token = tokenData.data;
+  //       localStorage.setItem("token", token);
+  //       console.log("Token: " + token);
+  //     }
 
-      navigate("/");
-    } catch (e) {
-      console.log(e);
-    }
-  }
+  //     navigate("/");
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // }
 
   return (
-    <div className="register-page">
-      <div className="register-page__left">
-        <p>login Form</p>
-        <form onSubmit={handleSubmit(onSubmit)} className="register-form">
-          <div className="register-form__element">
-            <label htmlFor="">Username</label>
-            <input {...register("username")} />
-          </div>
-          <div className="register-form__element">
-            <label htmlFor="">Password</label>
-            <input {...register("password")} />
-          </div>
-          <input type="submit" className="button" />
-        </form>
+    <div className="login-container">
+      <div className="login-functional">
+        <div className="login-navigation">
+          <h2>
+            <a href="/">Home</a>
+          </h2>
+        </div>
+        <div className="login-form">
+          <form action="">
+            <label htmlFor="">test</label>
+            <input type="text" />
+          </form>
+        </div>
       </div>
-      <div className="register-page__left">
-        <p>right</p>
+      <div className="login-decorative">
+        <div className="login-decorative__img-container"></div>
       </div>
     </div>
   );
--- dorm-assist-website/src/pages/login.jsx
+++ dorm-assist-website/src/pages/login.jsx
@@ -1,15 +1,71 @@
-import Navbar from "../components/Universal/navbar";
-import Footer from "../components/Universal/footer";
-import LoginContainer from "../components/LoginAndRegister/loginContainer";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-export default function Login() {
+export default function LoginForm() {
+  const { register, handleSubmit } = useForm();
+  const navigate = useNavigate();
+  const onSubmit = (data) => {
+    loginAndGetToken(data);
+  };
+
+  async function loginAndGetToken(body) {
+    console.log(body);
+
+    try {
+      const tokenData = await axios({
+        method: "post",
+        url: "http://localhost:8080/users/authenticate",
+        data: body,
+      });
+
+      // console.log(tokenData.headers.get("Content-Type"));
+      if (!tokenData.headers.get("Content-Type").includes("html")) {
+        const token = tokenData.data;
+        localStorage.setItem("token", token);
+        console.log("Token: " + token);
+        navigate("/");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   return (
-    <>
-      <Navbar />
-      <main>
-        <LoginContainer page="login" />
-      </main>
-      <Footer />
-    </>
+    <div className="register-page">
+      <div className="register-page__left">
+        <p>login Form</p>
+        <form onSubmit={handleSubmit(onSubmit)} className="register-form">
+          <div className="register-form__element">
+            <label htmlFor="">Username</label>
+            <input {...register("username")} />
+          </div>
+          <div className="register-form__element">
+            <label htmlFor="">Password</label>
+            <input {...register("password")} />
+          </div>
+          <input type="submit" className="button" />
+        </form>
+      </div>
+      <div className="register-page__left">
+        <p>right</p>
+      </div>
+    </div>
   );
 }
+
+// import Navbar from "../components/Universal/navbar";
+// import Footer from "../components/Universal/footer";
+// import LoginContainer from "../components/LoginAndRegister/loginContainer";
+
+// export default function Login() {
+//   return (
+//     <>
+//       {/* <Navbar /> */}
+//       <main>
+//         <LoginContainer page="login" />
+//       </main>
+//       <Footer />
+//     </>
+//   );
+// }

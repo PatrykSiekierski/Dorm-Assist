c73a1c0e752163a77317f060f0870cfea1dfbc91
TruKoaiu <patryk.siekierski03@gmail.com>
2025-02-04 18:16:20 +0100

    Added loading page
    div that shows 3 jumping dots.
    Usage for admin panel when getting info if user should access it.

---
+++ dorm-assist-website/src/components/Utils/loadingScreen.jsx
@@ -0,0 +1,9 @@
+export default function LoadingScreen() {
+  return (
+    <div className="loading">
+      <span className="test"></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+}
--- dorm-assist-website/src/pages/adminPanel.jsx
+++ dorm-assist-website/src/pages/adminPanel.jsx
@@ -3,33 +3,50 @@ import Footer from "../components/Universal/footer";
 import MainAdmin from "../components/AdminPanel/mainAdmin";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { isAdmin } from "../components/Utils/authService";
+import LoadingScreen from "../components/Utils/loadingScreen";
 
 export default function AdminPanel() {
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
+  const [verified, setVerified] = useState(false);
 
   useEffect(() => {
     const verifyAdmin = async () => {
       const hasAdminAccess = await isAdmin(token);
       if (!hasAdminAccess) {
         navigate("/");
+      } else {
+        setVerified(true);
       }
     };
 
     verifyAdmin();
   }, [navigate, token]);
 
-  return (
-    <>
-      <header>
-        <Navbar />
-      </header>
-      <main>
-        <MainAdmin />
-      </main>
-      <Footer />
-    </>
-  );
+  if (verified) {
+    return (
+      <>
+        <header>
+          <Navbar />
+        </header>
+        <main>
+          <MainAdmin />
+        </main>
+        <Footer />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <header>
+          <Navbar />
+        </header>
+        <main>
+          <LoadingScreen />
+        </main>
+      </>
+    );
+  }
 }

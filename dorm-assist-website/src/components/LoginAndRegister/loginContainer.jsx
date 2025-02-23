b9574a395c3d3abd996c3f5ae9162cc62b905f7d
TruKoaiu <patryk.siekierski03@gmail.com>
2025-02-23 14:42:13 +0100

    Tweaks

--- dorm-assist-website/src/components/LoginAndRegister/loginContainer.jsx
+++ dorm-assist-website/src/components/LoginAndRegister/loginContainer.jsx
@@ -1,17 +1,26 @@
+import { useNavigate } from "react-router-dom";
 import LoginForm from "./loginForm";
 
 export default function LoginContainer({ page }) {
+  const navigate = useNavigate();
+
+  function goToRegisterPage() {
+    navigate("/register");
+  }
+
   return (
     <section id="info-section">
       <div className="grid-form border">
         <div id="icon-side">
-          <img src="src/assets/form-icon.svg" alt="form icon" />
+          {/* <img src="src/assets/form-icon.svg" alt="form icon" /> */}
           <h2>Nie masz konta?</h2>
           <p>Stwórz nowe konto:</p>
-          <button className="button">Zarejestruj</button>
+          <button className="button" onClick={goToRegisterPage}>
+            Zarejestruj
+          </button>
         </div>
         <div className="form-side">
-          <h2>Zgłoś problem:</h2>
+          <h2>Logowanie:</h2>
           <LoginForm />
         </div>
       </div>
--- dorm-assist-website/src/components/LoginAndRegister/registerContainer.jsx
+++ dorm-assist-website/src/components/LoginAndRegister/registerContainer.jsx
@@ -1,14 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import RegisterForm from "./registerForm";
 
 export default function RegisterContainer() {
+  const navigate = useNavigate();
+
+  function goToLoginPage() {
+    navigate("/login");
+  }
+
   return (
     <section id="info-section">
       <div className="grid-form border">
         <div id="icon-side">
-          <img src="src/assets/form-icon.svg" alt="form icon" />
+          {/* <img src="src/assets/form-icon.svg" alt="form icon" /> */}
           <h2>Masz już konto?</h2>
           <p>Przejdź do logowania przyciskiem poniżej!</p>
-          <button className="button">Logowanie</button>
+          <button className="button" onClick={goToLoginPage}>
+            Logowanie
+          </button>
         </div>
         <div className="form-side">
           <h2>Rejestracja:</h2>

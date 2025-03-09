72f24a516150d37ef6fb85360ab3f04ce87d4fa1
TruKoaiu <patryk.siekierski03@gmail.com>
2025-03-09 18:50:03 +0100

    Added repo function

--- dorm-assist-api/src/main/java/me/dormdesk/api/repository/ExampleUserRepo.java
+++ dorm-assist-api/src/main/java/me/dormdesk/api/repository/ExampleUserRepo.java
@@ -1,12 +1,16 @@
 package me.dormdesk.api.repository;
 
 import me.dormdesk.api.model.ExampleUsersData;
+import me.dormdesk.api.model.UserData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ExampleUserRepo extends JpaRepository<ExampleUsersData, Integer> {
 
     List<ExampleUsersData> findByUserId(int userId);
+    Optional<ExampleUsersData> findByEmail(String email);
+
 }

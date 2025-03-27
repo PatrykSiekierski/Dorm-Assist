a0f3e1a162e9ff42d0357baca49dd72534c18dbd
TruKoaiu <patryk.siekierski03@gmail.com>
2025-03-27 22:40:31 +0100

    Login requirement message
    Added message that you can send reports only after loging in.

--- dorm-assist-website/src/components/Report/info.jsx
+++ dorm-assist-website/src/components/Report/info.jsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
+import { isAdmin } from "../Utils/authService";
 import Form from "./form";
 
 export default function Info() {
+  const [verified, setVerified] = useState(false);
+
+  useEffect(() => {
+    const verifyAdmin = async () => {
+      const hasAdminAccess = await isAdmin();
+      if (hasAdminAccess) {
+        setVerified(true);
+      } else {
+        setVerified(false);
+      }
+    };
+
+    verifyAdmin();
+  }, []);
+
   return (
     <section id="info-section">
       <div className="grid-form border">
@@ -11,6 +28,13 @@ export default function Info() {
             Dzięki tym informacjom możemy zawęzić krąg podejrzeń co poszło nie
             tak i szybciej udzielić pomocy.
           </p>
+          {!verified ? (
+            <p className="error-message">
+              Aby zgłosić problem należy być zalogowanym.
+            </p>
+          ) : (
+            ""
+          )}
         </div>
         <div className="form-side">
           <h2>Zgłoś problem:</h2>

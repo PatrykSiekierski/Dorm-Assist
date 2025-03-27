8437f01f28d582e0ea32d822941fb6aa1d07be1b
TruKoaiu <patryk.siekierski03@gmail.com>
2025-03-27 20:58:29 +0100

    Responsive addition for navbar

--- dorm-assist-website/src/components/Universal/navbar.jsx
+++ dorm-assist-website/src/components/Universal/navbar.jsx
@@ -10,6 +10,8 @@ import { isAdmin } from "../Utils/authService";
 export default function Navbar() {
   const navigate = useNavigate();
 
+  const [navbarState, setNavbarState] = useState(false);
+
   const isUserLoggedIn = isTokenValid();
   const role = getUserRole();
 
@@ -36,12 +38,19 @@ export default function Navbar() {
     verifyAdmin();
   }, []);
 
+  function toggleNavbar() {
+    setNavbarState(!navbarState);
+  }
+
   return (
     <nav>
       <a href="/">
         <img src="src/assets/cable-lan-svgrepo-com.svg" alt="Logo" />
       </a>
-      <ul>
+      <button className="navbar-extender" onClick={toggleNavbar}>
+        <img src="src/assets/list-svg.svg" alt="Logo" />
+      </button>
+      <ul className={"navbar-links"}>
         <li>
           <a href="/#faq-article">Pytania</a>
         </li>
@@ -54,7 +63,8 @@ export default function Navbar() {
           </li>
         )}
       </ul>
-      <ul>
+
+      <ul className={"navbar-user"}>
         {!isUserLoggedIn ? (
           <>
             <li>
@@ -71,7 +81,42 @@ export default function Navbar() {
                 Wyloguj
               </a>
             </li>
-            <li style={{ marginLeft: "1rem" }}>
+            <li>
+              <a href="/settings">Profil</a>
+            </li>
+          </>
+        )}
+      </ul>
+
+      <ul className={"navbar-small" + (navbarState ? "" : " hide")}>
+        <li>
+          <a href="/#faq-article">Pytania</a>
+        </li>
+        <li>
+          <a href="/report">Zgłoś Online</a>
+        </li>
+        {verified && (
+          <li>
+            <a href="/admin">Admin panel</a>
+          </li>
+        )}
+        {!isUserLoggedIn ? (
+          <>
+            <li>
+              <a href="/login">Zaloguj</a>
+            </li>
+            <li className="border-special">
+              <a href="/register">Zarejestruj</a>
+            </li>
+          </>
+        ) : (
+          <>
+            <li className="border-special">
+              <a href="/" onClick={logOut}>
+                Wyloguj
+              </a>
+            </li>
+            <li>
               <a href="/settings">Profil</a>
             </li>
           </>

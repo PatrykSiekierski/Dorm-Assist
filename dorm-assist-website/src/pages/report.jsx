9f30b5d286a80f3eadc11d0f732896a2b540a28e
TruKoaiu <patryk.siekierski03@gmail.com>
2025-04-17 12:52:23 +0200

    Needs to be logged in now to see this page

--- dorm-assist-website/src/pages/report.jsx
+++ dorm-assist-website/src/pages/report.jsx
@@ -1,15 +1,49 @@
 import Navbar from "../components/Universal/navbar";
 import Footer from "../components/Universal/footer";
 import Info from "../components/Report/info";
+import { useEffect, useState } from "react";
+import { isAdmin } from "../components/Utils/authService";
+import { useNavigate } from "react-router-dom";
+import LoadingScreen from "../components/Utils/loadingScreen";
 
 export default function Report() {
-  return (
-    <>
-      <Navbar />
-      <main>
-        <Info />
-      </main>
-      <Footer />
-    </>
-  );
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+  const [verified, setVerified] = useState(false);
+
+  useEffect(() => {
+    const verifyAdmin = async () => {
+      const hasAdminAccess = await isAdmin(token);
+      if (!hasAdminAccess) {
+        navigate("/login");
+      } else {
+        setVerified(true);
+      }
+    };
+
+    verifyAdmin();
+  }, [navigate, token]);
+
+  if (verified) {
+    return (
+      <>
+        <Navbar />
+        <main>
+          <Info />
+        </main>
+        <Footer />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <header>
+          <Navbar />
+        </header>
+        <main>
+          <LoadingScreen />
+        </main>
+      </>
+    );
+  }
 }
